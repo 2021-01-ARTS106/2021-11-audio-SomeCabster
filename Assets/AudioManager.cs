e7be7dc536c0013a230b6e6b423ff35468eef81f
SomeCabster <SomeCabster@users.noreply.github.com>
2021-04-20 01:11:19 -0400

    Attempt Audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour{
+    public static AudioManager manager;
+
+    private voidd Awake()
+    {
+        manager = this;
+    }
+
+    public AudioSource mainMusic;
+    public AudioSource auxMusic;
+    public AudioSource ambSound;
+
+
+}
